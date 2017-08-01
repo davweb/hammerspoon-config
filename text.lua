bd67e865a2d109adb541a7f0f3db131e0dcb1f58
David Webb <dave@davidwebb.me.uk>
2017-08-01 12:55:05 +0100

    Shortcuts for pasting special characters.

--- text.lua
+++ text.lua
@@ -1,30 +1,16 @@
--- perform a paste using ⌘+V
 local function paste() 
   hs.eventtap.keyStroke({"cmd"}, "v")
 end
 
--- return a function that types the specified text
-local function type(text)
-  return function()
-    hs.eventtap.keyStrokes(text)
-  end
-end
-
--- returns a function that pastes the specified text for when hs.eventtap.keyStrokes fails
 local function pasteText(text)
   return function()
     local oldContents = hs.pasteboard.getContents()
     hs.pasteboard.setContents(text)
     paste()
-    
-    -- workaround to ensure we don't put the old clipboard contents back before the paste happens
-    hs.timer.doAfter(0.1, function ()
-      hs.pasteboard.setContents(oldContents)
-    end)
+    hs.pasteboard.setContents(oldContents)
   end
 end
 
 return {
-  type = type,
   paste = pasteText
 }
