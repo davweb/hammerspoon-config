08335511ab5e972c153e206704c9b9fac2dc3bdb
David Webb <dave@davidwebb.me.uk>
2017-09-21 11:36:31 +0100

    1. Make tidy take an argument rather have forceTidy as a separate function 2. Use hs.eventtap.keyStroke for character hotkeys where possible 3. Fix bug with clipboard contents being put back before a hotkey paste happens 4. Some new config

--- text.lua
+++ text.lua
@@ -1,16 +1,30 @@
+-- perform a paste using ⌘+V
 local function paste() 
   hs.eventtap.keyStroke({"cmd"}, "v")
 end
 
+-- return a function that types the specified text
+local function type(text)
+  return function()
+    hs.eventtap.keyStrokes(text)
+  end
+end
+
+-- returns a funcition that pastes the specified text for when hs.eventtap.keyStrokes fails
 local function pasteText(text)
   return function()
     local oldContents = hs.pasteboard.getContents()
     hs.pasteboard.setContents(text)
     paste()
-    hs.pasteboard.setContents(oldContents)
+    
+    -- workaround to ensure we don't put the old clipboard contents back before the paste happens
+    hs.timer.doAfter(0.1, function ()
+      hs.pasteboard.setContents(oldContents)
+    end)
   end
 end
 
 return {
+  type = type,
   paste = pasteText
 }
