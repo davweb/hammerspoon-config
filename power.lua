22aa9a47d2c1eae715fa9aa6cff0a6d0f7150b4a
David Webb <dwebb@tripadvisor.com>
2018-03-26 19:03:09 +0100

    Add power watchiing
    Watch for switches between battery and AC power, and kill some GPU forcing apps when switching to battery.

---
+++ power.lua
@@ -0,0 +1,44 @@
+local function mainsPower()
+    print("AC Power")
+    -- Nothing currently
+end
+
+local function killApp(name)
+    local app = hs.application.get(name)
+
+    if app ~= nil then
+        app:kill()
+    end
+end
+
+local function batteryPower()
+    print("Battery Power")
+
+    -- Kill Apps that force Discrete GPU when switching to battery power
+    killApp('FreeChat for Facebook Messenger')
+    killApp('Spotify')
+end
+
+local function powerChanged()
+    if powerSource == 'AC Power' then
+        mainsPower()
+    elseif powerSource == 'Battery Power' then
+        batteryPower()
+    end
+end
+
+local function batteryChanged() 
+    local currentPowerSource = hs.battery.powerSource()
+    
+    if (currentPowerSource ~= powerSource) then
+        powerSource = currentPowerSource
+        powerChanged()
+    end
+end
+
+-- Store powerSource in a global because we want to remember it
+powerSource = ""
+
+-- Store battery watcher in a global variable so it doesn't get garbage collected
+batteryWatcher = hs.battery.watcher.new(batteryChanged) 
+batteryWatcher:start()
