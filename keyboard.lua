3f0c24800742a3cec468d2286c182c37b9f66710
David Webb <dave@davidwebb.me.uk>
2022-05-28 07:41:29 +0100

    Fix play button handling and removed unused code

--- keyboard.lua
+++ keyboard.lua
@@ -1,27 +1,10 @@
 -- luacheck: globals hs keyboardListener
 
 local types = hs.eventtap.event.types
-local appFilter = require("app-filters")
+local audioApps = {"Spotify", "VLC", "Overcast", "BBC Sounds"}
 
-local function currentApplication()
-  local focusedWindow = hs.window.focusedWindow()
-
-  if focusedWindow == nil then
-    return nil
-  else
-    return focusedWindow:application():name()
-  end
-end
-
-local function flagsAsText(flags)
-  local text = ""
-
-  for flag, _ in pairs(flags) do
-    text = text .. flag .. " "
-  end
-
-  return text
-end
+-- Window filter for audio apps that we'll keep running without any filters
+local windowFilter = hs.window.filter.new(audioApps):keepActive()
 
 local function launchSpotify()
   hs.application.launchOrFocus("Spotify")
@@ -29,12 +12,15 @@ end
 
 -- By default Play launches iTunes if no music app is running. This makes it launch Spotify instead
 local function handlePlay(down)
+
   if not down then
     return false
   end
 
-  if appFilter.isRunning("Spotify", "VLC", "iPlayer Radio") then
-    -- If there are any audio player windows we can do nothing
+  local windows = windowFilter:getWindows()
+
+  -- If there are any audio player windows we can do nothing
+  if next(windows) ~= nil then
     return false
   end
 
@@ -43,9 +29,6 @@ local function handlePlay(down)
 end
 
 local function handleKey(key, down, flags)
-  -- print(key, down, flagsAsText(flags))
-  -- local app = currentApplication()
-
   if key == 'PLAY' then
     return handlePlay(down)
   end
@@ -54,15 +37,15 @@ local function handleKey(key, down, flags)
 end
 
 local function keyPressed(event)
-  if event:getType() == types.keyDown or event:getType() == types.keyUp then
-    local fnKey = hs.keycodes.map[event:getKeyCode()]
+  -- if event:getType() == types.keyDown or event:getType() == types.keyUp then
+  --   local fnKey = hs.keycodes.map[event:getKeyCode()]
 
-    if fnKey then
-      return handleKey(fnKey, event:getType() == types.keyDown, event:getFlags())
-    end
-  end
+  --   if fnKey then
+  --     return handleKey(fnKey, event:getType() == types.keyDown, event:getFlags())
+  --   end
+  -- end
 
-  if event:getType() == types.NSSystemDefined then
+  if event:getType() == types.systemDefined then
     local data = event:systemKey()
 
     if data.key ~= nil and data.down ~= nil then
@@ -73,12 +56,6 @@ local function keyPressed(event)
   return false
 end
 
--- Initialise appFilters
-appFilter.get("Spotify")
-appFilter.get("VLC")
-appFilter.get("iPlayer Radio")
-appFilter.get("Sonos")
-
 -- Store keyboard listener in a global variable so it doesn't get garbage collected
-keyboardListener = hs.eventtap.new({types.keyDown, types.keyUp, types.NSSystemDefined}, keyPressed)
+keyboardListener = hs.eventtap.new({types.systemDefined}, keyPressed)
 keyboardListener:start()
